597bbc5b4d7d1ac8f36be589486d098086e33ec1
Kika7536 <104264433+Kika7536@users.noreply.github.com>
2024-09-21 18:34:44 +0300

    Добавьте файлы проекта.

---
+++ консоля.cpp
@@ -0,0 +1,46 @@
+﻿#include<iostream>
+using namespace std;
+
+
+void main() {
+	setlocale(LC_ALL, "rus");
+
+	cout << "Задание 7. Сокращение дроби." << endl;
+	cout << "--------------------------------------------" << endl << endl;
+
+	int m, n;
+	int i = 20;
+
+
+	while (true) {
+		cout << "Введите числитель: ";
+		cin >> m;
+
+		cout << "Введите знаменатель: ";
+		cin >> n;
+
+		if ((m < 0 && n < 0) || (m > 0 && n < 0)) {
+			m *= -1;
+			n *= -1;
+		}
+
+		if (n == 0) {
+			cout << "Знаменатель не может быть равным 0." << endl;
+			main();
+		}
+
+
+		while (i != 1) {
+			if (n % i == 0 && m % i == 0) {
+				n /= i;
+				m /= i;
+			}
+			i--;
+
+		}
+
+		cout << "Результат: " << m << " / " << n << endl;
+
+		i = 20;
+	}
+}
